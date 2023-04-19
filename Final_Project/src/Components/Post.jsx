c600e243b016054e9ce24a0ef5c8a84729ba5bf7
unknown <shresthayazu@gmail.com>
2023-04-19 04:06:24 -0500

    correct post vertical alignment

--- Final_Project/src/Components/Post.jsx
+++ Final_Project/src/Components/Post.jsx
@@ -43,9 +43,9 @@ function Post(props) {
   return (
     <div className="px-40 my-12">
       <div
-        className={`border-2 border-gray-600 bg-gray-800 p-9 pr-5 group hover:border-gray-400 duration-300 rounded-xl flex flex-1 cursor-pointer `}
+        className={`border-2 border-gray-600  bg-gray-800 p-4  pr-5 group hover:border-gray-400 duration-300 rounded-xl flex flex-1 cursor-pointer `}
       >
-        <div className="text-center w-11/12 border-r-2 border-gray-600 group-hover:border-gray-400 duration-300 select-none">
+        <div className="text-center w-11/12  m-auto border-r-2 border-gray-600 group-hover:border-gray-400 duration-300 select-none">
           {Data ? (
             <h1 className="text-3xl font-bold">{Data.Title}</h1>
           ) : (
