39856c9ad8f755a565a8193a804178188dd90520
unknown <shresthayazu@gmail.com>
2023-04-19 04:01:29 -0500

    add creat post and up/down vote logic

--- Final_Project/src/Components/Body.jsx
+++ Final_Project/src/Components/Body.jsx
@@ -1,34 +1,62 @@
-import React, { useEffect } from 'react'
-import {createClient} from '@supabase/supabase-js'
-import Post from './Post'
-import { useState } from 'react'
-
+import React, { useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+import Post from "./Post";
+import { useState } from "react";
 
 function Body() {
-    const supabaseURL = 'https://kstbhpzxvjyfrtccomtm.supabase.co'
-    const supabasekey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzdGJocHp4dmp5ZnJ0Y2NvbXRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE3MTI4ODIsImV4cCI6MTk5NzI4ODg4Mn0.Tk8PQTkbHHJIEbghy8mUCS42CM7sokJTwiBbLx8WxRo'
+  const supabaseURL = "https://kstbhpzxvjyfrtccomtm.supabase.co";
+  const supabasekey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzdGJocHp4dmp5ZnJ0Y2NvbXRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE3MTI4ODIsImV4cCI6MTk5NzI4ODg4Mn0.Tk8PQTkbHHJIEbghy8mUCS42CM7sokJTwiBbLx8WxRo";
 
-    const [data, setData]= useState([])
+  const [data, setData] = useState([]);
+  const[title, setTitle] = useState('')
+    const[text, setText] = useState('')
 
-    const supabase = createClient(supabaseURL, supabasekey)
+  const supabase = createClient(supabaseURL, supabasekey);
 
-    const fetchData = async () => {
-        const {data, error} = await supabase.from('Data').select('*')
-        // console.log(data)
-        setData(data)
-    }
+  const fetchData = async () => {
+    const { data, error } = await supabase.from("Data").select("*");
+    // console.log(data)
+    setData(data);
+  };
 
-    useEffect(() => {
-        fetchData()
-    }, [])
-    
-    const test= data[0]
-    console.log(test)
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const test = data[0];
+  console.log(test);
+
+  const handleClick = async (e) => {
+    e.preventDefault()
+    const { data, error } = await supabase
+      .from("Data")
+      .insert([{ Title: title, Text: text }]);
+    console.log(data);
+    setTitle('')
+    setText('')
+  }
   return (
-    <div className='px-40 py-12'>
-        <Post data={test} />
-    </div>
-  )
+    <>
+        <div className="px-40 pt-12">
+            <div className="border-2 border-teal-700 bg-gray-800 px-9 py-4 pr-5 group hover:border-gray-400 duration-300 rounded-xl flex flex-1 cursor-pointer">
+            <form action="" className="w-full">
+                <label className="text-lg font-semibold">Title</label>
+                <input type="text" value={title} className="w-full border-2 text-center border-gray-500 focus:outline-none focus:border-teal-500 rounded-xl px-3 py-1 mb-3" onChange={(e) => setTitle(e.target.value)}/>
+                <label className="text-lg font-semibold">Post</label>
+                <textarea type="text" value={text} className="w-full border-2 border-gray-500 focus:outline-none focus:border-teal-500 rounded-xl px-3 py-1"   onChange={(e) => setText(e.target.value)}/>
+                <button className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-6 rounded-full" onClick={handleClick}>Post</button>
+            </form>
+            </div>
+        </div>
+      {
+        data.map((item, index) => {
+          console.log(item);
+          return <Post key={index} prop={item} />;
+        }
+      )}
+    </>
+  );
 }
 
-export default Body
+export default Body;
--- Final_Project/src/Components/Post.jsx
+++ Final_Project/src/Components/Post.jsx
@@ -1,50 +1,83 @@
-import React from "react";
-import {BiUpvote, BiDownvote} from "react-icons/bi";
-import { useState } from "react";
+import React, { useState, useRef } from "react";
+import { BiUpvote, BiDownvote } from "react-icons/bi";
 
 function Post(props) {
-  const Data = props.data;
-  console.log("new Data", props);
+  console.log("what I get", props)
+  const Data = props.prop;
   const [vote, setVote] = useState(0);
-  const [clickUp, setClickUp] = useState(false);
-  const [clickDown, setClickDown] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
 
-  const handleClickUp = (e) => {
+  let btnUpRef = useRef();
+  let btnDownRef = useRef();
 
-    e.currentTarget.disabled = true;
-    if (clickUp) {
-      setVote(vote - 1);
-      clickUp = false;
+  const handleClickUp = () => {
+    if (!selectedOption) {
+      setSelectedOption("upvote");
+      setVote(1);
+    } else if (selectedOption === "downvote") {
+      setSelectedOption("upvote");
+      setVote(vote + 2);
+      btnDownRef.current.removeAttribute("disabled");
     } else {
-      setVote(vote + 1);
-      clickUp = true;
+      setSelectedOption("");
+      setVote(0);
     }
-  }
+    btnUpRef.current.setAttribute("disabled", "disabled");
+  };
 
   const handleClickDown = () => {
-    if (clickDown) {
-      setVote(vote + 1);
-      clickDown = false;
+    if (!selectedOption) {
+      setSelectedOption("downvote");
+      setVote(-1);
+    } else if (selectedOption === "upvote") {
+      setSelectedOption("downvote");
+      setVote(vote - 2);
+      btnUpRef.current.removeAttribute("disabled");
     } else {
-      setVote(vote - 1);
-      clickDown = true;
+      setSelectedOption("");
+      setVote(0);
     }
-  }
+    btnDownRef.current.setAttribute("disabled", "disabled");
+  };
 
   return (
-    <div className="border-2 border-gray-600 bg-gray-800 p-9 pr-5 group hover:border-gray-400 duration-300 rounded-xl flex flex-1 cursor-pointer ">
-      <div className="text-center w-11/12 border-r-2 border-gray-600 group-hover:border-gray-400 duration-300">
-        {Data ? (
-          <h1 className="text-3xl font-bold">{Data.Title}</h1>
-        ) : (
-          <h1>no data</h1>
-        )}
-        {Data ? <h1 className="text-lg font-medium">{Data.Text}</h1> : <h1>no data</h1>}
-      </div>
-      <div className="text-center ml-3">
-        <BiUpvote className="text-4xl text-gray-600 hover:text-gray-400 duration-300 " onClick={handleClickUp}/>
-        {<p className="text-base">{vote}</p>}
-        <BiDownvote className="text-4xl text-gray-600 hover:text-gray-400 duration-300 " onClick={handleClickDown}/>
+    <div className="px-40 my-12">
+      <div
+        className={`border-2 border-gray-600 bg-gray-800 p-9 pr-5 group hover:border-gray-400 duration-300 rounded-xl flex flex-1 cursor-pointer `}
+      >
+        <div className="text-center w-11/12 border-r-2 border-gray-600 group-hover:border-gray-400 duration-300 select-none">
+          {Data ? (
+            <h1 className="text-3xl font-bold">{Data.Title}</h1>
+          ) : (
+            <h1>no data</h1>
+          )}
+          {Data ? (
+            <h1 className="text-lg font-medium">{Data.Text}</h1>
+          ) : (
+            <h1>no data</h1>
+          )}
+        </div>
+        <div className="text-center ml-3">
+          <BiUpvote
+            ref={btnUpRef}
+            className={`text-4xl text-gray-600 hover:text-gray-400 duration-300 ${
+              selectedOption === "upvote"
+                ? "text-teal-500 hover:text-teal-700"
+                : ""
+            }`}
+            onClick={handleClickUp}
+          />
+          <p className="text-base select-none">{vote}</p>
+          <BiDownvote
+            ref={btnDownRef}
+            className={`text-4xl text-gray-600 hover:text-gray-400 duration-300 ${
+              selectedOption === "downvote"
+                ? "text-red-600 hover:text-red-700"
+                : ""
+            }`}
+            onClick={handleClickDown}
+          />
+        </div>
       </div>
     </div>
   );
