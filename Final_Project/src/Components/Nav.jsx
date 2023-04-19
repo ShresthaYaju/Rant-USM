be3a7889d08c8b857da479a4e50a3b772173b1c9
Yaju Shrestha <shresthayazu@gmail.com>
2023-04-19 02:27:06 -0500

    add Post Component

--- Final_Project/src/Components/Nav.jsx
+++ Final_Project/src/Components/Nav.jsx
@@ -5,9 +5,9 @@ function Nav() {
     
   return (
   <>
-    <nav className='w-full bg-[#1A1A1B] border-b-2 border-gray-600 text-base flex flex-1 px-10 py-3 justify-end gap-6 font-bold'>
-         <button className='py-1 px-4 border-2 border-gray-700 rounded-3xl hover:bg-gray-500 duration-500'>Sign Up</button>
-         <button className='py-1 px-4 border-2 border-gray-800 rounded-3xl bg-teal-700 hover:bg-teal-500 duration-500'>Login</button>
+    <nav className='w-full bg-[#1A1A1B] border-b-2 border-gray-600 text-base flex flex-1 px-10 py-3 justify-end gap-4 font-bold'>
+         <button className='py-1 px-4 border-2 bg-[#484f58] border-gray-500 rounded-3xl hover:bg-gray-700 hover:border-teal-500 duration-500'>Sign Up</button>
+         <button className='py-1 px-4 border-2 border-gray-800 rounded-3xl bg-teal-500 hover:bg-teal-700 hover:border-gray-300 duration-500'>Login</button>
     </nav>
     
     <div>
--- Final_Project/src/Components/Post.jsx
+++ Final_Project/src/Components/Post.jsx
@@ -1,18 +1,50 @@
 import React from "react";
+import {BiUpvote, BiDownvote} from "react-icons/bi";
+import { useState } from "react";
 
 function Post(props) {
   const Data = props.data;
   console.log("new Data", props);
+  const [vote, setVote] = useState(0);
+  const [clickUp, setClickUp] = useState(false);
+  const [clickDown, setClickDown] = useState(false);
+
+  const handleClickUp = (e) => {
+
+    e.currentTarget.disabled = true;
+    if (clickUp) {
+      setVote(vote - 1);
+      clickUp = false;
+    } else {
+      setVote(vote + 1);
+      clickUp = true;
+    }
+  }
+
+  const handleClickDown = () => {
+    if (clickDown) {
+      setVote(vote + 1);
+      clickDown = false;
+    } else {
+      setVote(vote - 1);
+      clickDown = true;
+    }
+  }
 
   return (
-    <div className="border-2 border-gray-600 bg-gray-800 p-9 hover:scale-105 duration-500 rounded-xl flex flex-1 cursor-pointer ">
-      <div className="text-center w-11/12 border-r-2 border-gray-600">
+    <div className="border-2 border-gray-600 bg-gray-800 p-9 pr-5 group hover:border-gray-400 duration-300 rounded-xl flex flex-1 cursor-pointer ">
+      <div className="text-center w-11/12 border-r-2 border-gray-600 group-hover:border-gray-400 duration-300">
         {Data ? (
           <h1 className="text-3xl font-bold">{Data.Title}</h1>
         ) : (
           <h1>no data</h1>
         )}
         {Data ? <h1 className="text-lg font-medium">{Data.Text}</h1> : <h1>no data</h1>}
+      </div>
+      <div className="text-center ml-3">
+        <BiUpvote className="text-4xl text-gray-600 hover:text-gray-400 duration-300 " onClick={handleClickUp}/>
+        {<p className="text-base">{vote}</p>}
+        <BiDownvote className="text-4xl text-gray-600 hover:text-gray-400 duration-300 " onClick={handleClickDown}/>
       </div>
     </div>
   );
